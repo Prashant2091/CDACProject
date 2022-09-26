ca77dbdbec655cf4904120d85704c4462e9c53ea
Prashant2091 <97425883+Prashant2091@users.noreply.github.com>
2022-09-27 04:54:07 +0530

    Update App.py

--- App.py
+++ App.py
@@ -34,7 +34,7 @@ def weather(city):
     #humidity = (soup.select("#wob_hm")[0].getText().strip())
     #humidity=float(humidity.replace('%',""))
     temp = float(weather)*1.8
-    temp1 = round((float(temp)-56),2)
+    temp1 = round((float(temp)+32),2)
     st.write(location,time)
     #st.write(time)
     st.write("Temperature : ",str(temp1)+"°F") 
