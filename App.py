4fc4084a7495b0df8ccaead44eec360d7ac53a7e
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2022-09-28 20:48:37 +0530

    Update App.py

--- App.py
+++ App.py
@@ -136,6 +136,6 @@ if sidebar == "Developers":
 
 elif sidebar == "Guide":
     st.header("Guide")
-    st.image("promod_sir.jpg")
+    st.image("pramod_sir.jpg")
     st.header("""Pramod Kumar Sharma""")
     st.image("cdac_pune.jpg")
